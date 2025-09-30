ac75a9f321cba4eebeee3713145da43bbcf59aa2
NutcrackerCom <namber1999@mail.ru>
2025-09-30 12:59:51 +0300

    class string

---
+++ cpp-homework/hw_01/myString/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class String
+{
+public:
+    String()
+    {
+        str="";
+    }
+
+    String(char* s)
+    {
+        str = s;
+    }
+
+private:
+    char* str;
+};
+
+int main()
+{
+   //String s("sdsd"); 
+}
