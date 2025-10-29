e75211c3ac04fed24a711ef684bb977ad951323b
NutcrackerCom <namber1999@mail.ru>
2025-10-29 11:41:10 +0300

    Bubble Sort LL

--- cpp-homework/hw_03/148_sort_list/main.cpp
+++ cpp-homework/hw_03/148_sort_list/main.cpp
@@ -1,3 +1,5 @@
+// Bubble Sort 
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,35 +12,45 @@
      ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
 
- class Solution {
+class Solution {
 public:
     ListNode* sortList(ListNode* head) 
     {
         ListNode* current = head;
-        ListNode* next = current->next;
+        ListNode* next = nullptr;
         ListNode* prev = nullptr;
-        bool flag = true;
-        while (flag)
+        bool sorted = false;
+        while (!sorted)
         {
+            current = head;
             prev = nullptr;
-            flag = false;
+            sorted = true;
             while(current!=nullptr && current->next!=nullptr)
             {
                 next = current->next;
 
-                if(current->val < current->next->val)
+                if(current->val > current->next->val)
                 {
-                    flag = true;
+                    sorted = false;
+
                     current->next = next->next;
                     next->next = current;
-                    if(prev == nullptr) head = current;
-                    prev = current;
-                }
-            }
-        }
-        
 
-        
+                    if(prev == nullptr) head = next;
+                    else prev->next = next;
+
+                    prev = next;
+                }
+                else
+                {
+                    prev = current;
+                    current = current->next;
+                }
+                print(head);
+            }
+            std::cout<< sorted << std::endl;
+        }
+        return head;
     }
 
     void print(ListNode* head)
@@ -52,3 +64,17 @@ public:
         std::cout << std::endl;
     }
 };
+
+int main()
+{
+    //[-1,5,3,4,0]
+    ListNode a1 = 0;
+    ListNode a2 = {4, &a1};
+    ListNode a3 = {3, &a2};
+    ListNode a4 = {5, &a3};
+    ListNode a5 = {-1, &a4};
+    ListNode a6 = {6, &a5};
+    ListNode c = {7, &a6};
+    Solution s;
+    s.sortList(&a5);
+}
